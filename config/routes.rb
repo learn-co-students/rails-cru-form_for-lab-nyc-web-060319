ca14a51a6539d2f5e3c31bdc60c4bb8cc1eb3d56
Alexander Riccio <test35965+learn@gmail.com>
2019-06-28 15:27:05 -0400

    generate all the stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/update'
+
+  get 'songs/edit'
+
+  get 'songs/destroy'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/update'
+
+  get 'genres/edit'
+
+  get 'genres/destroy'
+
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/update'
+
+  get 'artists/edit'
+
+  get 'artists/destroy'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
